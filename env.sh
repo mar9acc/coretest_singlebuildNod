bc7f7ad84597b61b26cc0a596666e21bfe3b8d22
shiptest-rc-ow <shiptest.rc.ow@gmail.com>
2017-06-16 18:20:38 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,49 +3,49 @@
 if [[ "$TEST" == "FOO" ]]; then
   echo "TEST env matched value"
 else
-  echo ERROR: Failed, wrong value forthe TEST variable. Please recheck the env variables 
+  echo ERROR: Failed, wrong value forthe TEST variable. Please check the environment variable value inthe yml
   exit 1
 fi;
 
 if [[ "$secure" == "testingvariable" ]]; then
   echo "secure env matched value"
 else
-  echo ERROR: Failed, wrong value forthe secure variable. Please recheck the env variables 
+  echo ERROR: Failed, wrong value forthe secure variable. Please check the environment variable value inthe yml
   exit 1
 fi;
 
 if [[ "$M1" == "ONE" ]]; then
   echo "M1 env matched value"
 else
-  echo ERROR: Failed, wrong value forthe M1 variable. Please recheck the env variables 
+  echo ERROR: Failed, wrong value forthe M1 variable. Please check the environment variable value inthe yml
   exit 1
 fi;
 
 if [[ "$IMAGE_NAME" == "shippabledocker/sample_node_pvt" ]]; then
   echo "IMAGE_NAME env matched value"
 else
-  echo ERROR: Failed, wrong value forthe IMAGE_NAME variable. Please recheck the env variables 
+  echo ERROR: Failed, wrong value forthe IMAGE_NAME variable. Please check the environment variable value inthe yml
   exit 1
 fi;
 
 if [[ "$IMAGE_TAG" == "latest.40" ]]; then
   echo "IMAGE_TAG env matched value"
 else
-  echo ERROR: Failed, wrong value forthe IMAGE_TAG variable. Please recheck the env variables 
+  echo ERROR: Failed, wrong value forthe IMAGE_TAG variable. Please check the environment variable value inthe yml
   exit 1
 fi;
 
 if [[ "$IMAGE_PULL" == "true" ]]; then
   echo "IMAGE_PULL env matched value"
 else
-  echo ERROR: Failed, wrong value forthe IMAGE_PUL variable. Please recheck the env variables 
+  echo ERROR: Failed, wrong value forthe IMAGE_PUL variable. Please check the environment variable value inthe yml
   exit 1
 fi;
 
 if [[ "$CUSTOM" == "ymlenv" ]]; then
   echo "CUSTOM env matched value"
 else
-  echo ERROR: Failed, wrong value forthe CUSTOM variable. Please recheck the env variables 
+  echo ERROR: Failed, wrong value forthe CUSTOM variable. Please check the environment variable value inthe yml
   exit 1
 fi;
 
@@ -53,14 +53,14 @@ fi;
 if [[ "$key1" == "value1" ]]; then
   echo "key1 env matched value"
 else
-  echo ERROR: Failed, wrong value forthe key1 variable. Please recheck the env variables 
+  echo ERROR: Failed, wrong value forthe key1 variable. Please check the environment variable value inthe yml
   exit 1
 fi;
 
 if [[ "$key2" == "value2" ]]; then
   echo "key2 env matched value"
 else
-  echo ERROR: Failed, wrong value forthe key2 variable. Please recheck the env variables 
+  echo ERROR: Failed, wrong value forthe key2 variable. Please check the environment variable value inthe yml
   exit 1
 fi;
 
