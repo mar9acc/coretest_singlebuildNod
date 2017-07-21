937e4c55ba6944a9860e99b811a9c7923857e81a
shiptest-rc-ow <shiptest.rc.ow@gmail.com>
2017-07-21 13:21:30 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -10,12 +10,12 @@ echo NODE_VERSION=$NODE_VERSION
 #export NODE_VERSION=$SHIPPABLE_NODE_VERSION
 #echo NODE_VERSION=$NODE_VERSION
 
-if [[ "$NODE_VERSION" == "0.10" ]]; then
-  echo "TEST env matched value"
-else
-  echo "ERROR: Failed, wrong value for the NODE_JS version variable. Please check the environment variable value in the yml"
-  exit 1
-fi;
+#if [[ "$NODE_VERSION" == "0.10" ]]; then
+ # echo "TEST env matched value"
+#else
+ # echo "ERROR: Failed, wrong value for the NODE_JS version variable. Please check the environment variable value in the yml"
+ # exit 1
+#fi;
 
 if [[ "$TEST" == "FOO" ]]; then
   echo "TEST env matched value"
